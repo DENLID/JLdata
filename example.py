2a8b9d7efb2c1ca3894ceb5acff9dc47e0f3f684
DENLID <127737950+DENLID@users.noreply.github.com>
2023-07-30 21:21:46 +0300

    Create example.py

---
+++ example.py
@@ -0,0 +1,20 @@
+from JLdata import JLD
+import Jldata as JL
+
+db = JLD(file="db.json")
+data = db.data
+
+""" before
+{
+  "name": ""
+}
+"""
+
+data[name] = "Denis"
+db.commit()
+
+""" after
+{
+  "name": "Denis"
+}
+"""
